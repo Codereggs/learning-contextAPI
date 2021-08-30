8d8bfb246e0263869b6734c897830e7a0074f352
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-08-30 14:46:10 -0300

    feat texts and handleLanguage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-const Header = ({ theme, handleTheme }) => {
+const Header = ({ theme, handleTheme, texts, handleLanguage }) => {
   return (
     <header className={theme}>
-      <h2>Mi Aplicación sin Context API</h2>
-      <h3>Mi cabecera</h3>
-      <select name="language">
+      <h2>{texts.headerTitle}</h2>
+      <h3>{texts.headerSubtitle}</h3>
+      <select name="language" onChange={handleLanguage}>
         <option value="es" default>
           ES
         </option>
@@ -16,7 +16,7 @@ const Header = ({ theme, handleTheme }) => {
         onClick={handleTheme}
         value="light"
       />
-      <label htmlFor="light">Claro</label>
+      <label htmlFor="light">{texts.headerLight}</label>
       <input
         type="radio"
         name="theme"
@@ -24,8 +24,11 @@ const Header = ({ theme, handleTheme }) => {
         onClick={handleTheme}
         value="dark"
       />
-      <label htmlFor="dark">Oscuro</label>
-      <button>Iniciar Sesión</button>
+      <label htmlFor="dark">{texts.headerDark}</label>
+      <button>
+        {texts.buttonLogin}
+        {texts.buttonLogout}
+      </button>
     </header>
   );
 };
--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -4,9 +4,40 @@ import Header from "./Header";
 import Main from "./Main";
 
 const initialTheme = "light";
+const initialLanguage = "es";
+const translations = {
+  es: {
+    headerTitle: "Mi aplicación SIN Context API",
+    headerSubtitle: "Mi cabecera",
+    headerLight: "Claro",
+    headerDark: "Oscuro",
+    buttonLogin: "Iniciar Sesión",
+    buttonLogout: "Cerrar Sesión",
+    mainWelcome: "Bienvenid@ invitad@",
+    mainHello: "Hola Usuari@",
+    mainContent: "Mi contenido principal",
+    footerTitle: "Mi pié de página",
+  },
+  en: {
+    headerTitle: "My application without Context API",
+    headerSubtitle: "My header",
+    headerLight: "Light",
+    headerDark: "Dark",
+    buttonLogin: "Login",
+    buttonLogout: "Logout",
+    mainWelcome: "Welcome Guest",
+    mainHello: "Hello User",
+    mainContent: "My main content",
+    footerTitle: "My footer",
+  },
+};
 
 const MyPage = () => {
   const [theme, setTheme] = useState(initialTheme);
+  const [language, setLanguage] = useState(initialLanguage);
+  const [texts, setTexts] = useState(translations[language]);
+  console.log(texts);
+
   const handleTheme = (e) => {
     console.log(e.target.value);
     if (e.target.value === "light") {
@@ -15,11 +46,17 @@ const MyPage = () => {
       setTheme("dark");
     }
   };
+  const handleLanguage = (e) => {};
   return (
     <div className="myPage">
-      <Header theme={theme} handleTheme={handleTheme} />
-      <Main theme={theme} />
-      <Footer theme={theme} />
+      <Header
+        theme={theme}
+        handleTheme={handleTheme}
+        texts={texts}
+        handleLanguage={handleLanguage}
+      />
+      <Main theme={theme} texts={texts} />
+      <Footer theme={theme} texts={texts} />
     </div>
   );
 };
