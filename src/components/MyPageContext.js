11d770953cf3d7c818c32e60d3210c0b2acb88de
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-08-31 17:28:45 -0300

    feat language context

--- src/components/MyPageContext.js
+++ src/components/MyPageContext.js
@@ -1,54 +1,16 @@
 import { useState } from "react";
+import { LanguageProvider } from "../context/LanguageContext";
 import { ThemeProvider } from "../context/ThemeContext";
 import FooterContext from "./FooterContext";
 import HeaderContext from "./HeaderContext";
 import MainContext from "./MainContext";
 
-const initialLanguage = "es";
 const initialAuth = false;
-const translations = {
-  es: {
-    headerTitle: "Mi aplicación con Context API",
-    headerSubtitle: "Mi cabecera",
-    headerLight: "Claro",
-    headerDark: "Oscuro",
-    buttonLogin: "Iniciar Sesión",
-    buttonLogout: "Cerrar Sesión",
-    mainWelcome: "Bienvenid@ invitad@",
-    mainHello: "Hola Usuari@",
-    mainContent: "Mi contenido principal",
-    footerTitle: "Mi pié de página",
-  },
-  en: {
-    headerTitle: "My application withContext API",
-    headerSubtitle: "My header",
-    headerLight: "Light",
-    headerDark: "Dark",
-    buttonLogin: "Login",
-    buttonLogout: "Logout",
-    mainWelcome: "Welcome Guest",
-    mainHello: "Hello User",
-    mainContent: "My main content",
-    footerTitle: "My footer",
-  },
-};
 
 const MyPageContext = () => {
-  const [language, setLanguage] = useState(initialLanguage);
-  const [texts, setTexts] = useState(translations[language]);
   const [auth, setAuth] = useState(initialAuth);
   //console.log(texts);
 
-  const handleLanguage = (e) => {
-    // console.log(e.target.value);
-    if (e.target.value === "es") {
-      setLanguage("es");
-      setTexts(translations["es"]);
-    } else {
-      setLanguage("en");
-      setTexts(translations["en"]);
-    }
-  };
   const handleAuth = () => {
     if (auth) setAuth(false);
     else setAuth(true);
@@ -56,14 +18,11 @@ const MyPageContext = () => {
   return (
     <div className="myPage">
       <ThemeProvider>
-        <HeaderContext
-          texts={texts}
-          handleLanguage={handleLanguage}
-          auth={auth}
-          handleAuth={handleAuth}
-        />
-        <MainContext texts={texts} auth={auth} />
-        <FooterContext texts={texts} />
+        <LanguageProvider>
+          <HeaderContext auth={auth} handleAuth={handleAuth} />
+          <MainContext auth={auth} />
+          <FooterContext />
+        </LanguageProvider>
       </ThemeProvider>
     </div>
   );
